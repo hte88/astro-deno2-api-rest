40dfb16017278e48fb10b1def46c45036b76c0cf
hte88 <olivier.marion88@gmail.com>
2025-01-21 16:29:07 +0100

    feat: base components + layout style

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="bg-white">
+      <div
+        class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8"
+      >
+        <nav
+          class="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6"
+          aria-label="Footer"
+        >
+          <a href="/" class="text-gray-600 hover:text-gray-900">Home </a>
+          <a href="/register" class="text-gray-600 hover:text-gray-900"
+            >Register
+          </a>
+          <a href="/login" class="text-gray-600 hover:text-gray-900">Login </a>
+        </nav>
+
+        <p class="mt-10 text-center text-sm/6 text-gray-600">
+          &copy; 2024 Your Company, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+</template>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+      <nav class="flex w-full justify-center space-x-12 py-2">
+        <a href="/" class="text-gray-600 hover:text-gray-900">Home</a>
+        <a href="/register" class="text-gray-600 hover:text-gray-900"
+          >Register</a
+        >
+        <a href="/login" class="text-gray-600 hover:text-gray-900">Login</a>
+      </nav>
+    </header>
+</template>
